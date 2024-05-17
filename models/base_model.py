82c8be52e36f4a5837c978096ffcfb50c5206832
Mahmoud <mah2002moud@gmail.com>
2024-05-17 22:06:01 +0300

    Task 4 is done

--- models/base_model.py
+++ models/base_model.py
@@ -11,11 +11,19 @@ class BaseModel:
     A class that defines all common methods and attributes
     """
 
-    def __init__(self):
+    def __init__(self, *args, **kwargs):
         """Initializing the BaseModel class"""
         self.id = str(uuid.uuid4())
         self.created_at = datetime.datetime.now()
         self.updated_at = datetime.datetime.now()
+
+        if kwargs:
+            for key, value in kwargs.items():
+                if key == "__class__":
+                    continue
+                elif key == "created_at" or key == "updated_at":
+                    value = datetime.datetime.fromisoformat(value)
+                setattr(self, key, value)
 
     def __str__(self):
         """Returns the string form of the object"""
@@ -38,14 +46,24 @@ class BaseModel:
 if __name__ == "__main__":
 
     my_model = BaseModel()
-    my_model.name = "My First Model"
+    my_model.name = "My_First_Model"
     my_model.my_number = 89
+    print(my_model.id)
     print(my_model)
-    my_model.save()
-    print(my_model)
+    print(type(my_model.created_at))
+    print("--")
     my_model_json = my_model.to_dict()
     print(my_model_json)
     print("JSON of my_model:")
     for key in my_model_json.keys():
         print("\t{}: ({}) - {}".format(key, type(my_model_json[key]),
                                        my_model_json[key]))
+
+    print("--")
+    my_new_model = BaseModel(**my_model_json)
+    print(my_new_model.id)
+    print(my_new_model)
+    print(type(my_new_model.created_at))
+
+    print("--")
+    print(my_model is my_new_model)
