5cdb78ca4edc7c1a495cd1ccd0b9a4f0ebf0faf8
Mahmoud <mah2002moud@gmail.com>
2024-05-20 02:47:53 +0300

    this commit and the commit before it are for task 8

--- console.py
+++ console.py
@@ -1,8 +1,9 @@
 #!/usr/bin/python3
 """This module contains the command interpreter"""
 import cmd
-from models.base_model import BaseModel
 from models import storage
+from models.base_model import BaseModel
+from models.user import User
 
 
 class HBNBCommand(cmd.Cmd):
